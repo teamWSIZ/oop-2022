a38d4ee973bf43720603aa480ecb6f616ddaca9f
Piotr Marecki <pmarecki.pro@gmail.com>
2022-11-05 13:57:56 +0100

    feat: client write methods

--- src/z3_todo/prototypy/election.py
+++ src/z3_todo/prototypy/election.py
@@ -7,33 +7,80 @@ class Candidate:
         self.id = uuid4()
         self.others: list[Candidate] = []
         self.is_leader = None
+        self.leader = None
+        self.data = []
+
+
+    def write(self, message: str):
+        """
+        Funkcja do wywołania przez klientów
+        """
+        self.leader.leader_write(message)
+
+    def leader_write(self, message: str):
+        """
+        Funkcja do wywołania tylko przez leadera
+        """
+        self.data.append(message)
+        for oth in self.others:
+            oth.follower_write(message)
+
+    def follower_write(self, message: str):
+        """
+        Funkcja do wywołania tylko przez leadera
+        """
+        self.data.append(message)
+
+
+    def add_other(self, candidate: 'Candidate'):
+        self.others.append(candidate)
 
     def elect_leader(self):
         print(f'electing leader on {self.id}')
 
         # przerobić tą funkcję tak by self.is_leader było True, jeśli nasz id jest najmniejszy z wszystkich
-        if self.id < self.other.id:
+        min_id = self.id
+        for oth in self.others:
+            min_id = min(min_id, oth.id)
+
+        if self.id == min_id:
             self.is_leader = True
+            self.leader = self
         else:
             self.is_leader = False
 
         # czy to wystarczy ↓↓ by wybory odbyły się na wszystkich Candidate'ach?
-        if self.other.is_leader is None:
-            self.other.elect_leader()
+        for oth in self.others:
+            if oth.is_leader is None:
+                oth.elect_leader()
+            if oth.is_leader is True:
+                self.leader = oth
 
-
-    def __str__(self):
+    def __repr__(self):
         return f'Candidate(id={self.id}, is_leader={self.is_leader})'
 
 
-
 if __name__ == '__main__':
-    N = 3
-    candidates = [Candidate() for _  in range(N)]
+    N = 9
+    candidates = [Candidate() for _ in range(N)]
     for c in candidates:
-        print(c)
+        for ca in candidates:
+            if c != ca:
+                c.add_other(ca)
+
 
     candidates[0].elect_leader()
+    print(candidates)
+    print('-' * 10)
+
+    # klient wykonuje zapisy
+    candidates[1].write('aaa')
+    candidates[0].write('bbb')
+    candidates[4].write('ccc')
+    candidates[2].write('ddd')
+
+    for c in candidates[:3]:
+        print(c.data)
 
 
 # dla dwóch:
@@ -53,9 +100,6 @@ if __name__ == '__main__':
 # print(cand2)
 
 
-
-
-
 """
 1) Kandydaci powinni wiedzieć o sobie, 
 2) Powinni mieć pole is_leader: bool
@@ -63,4 +107,3 @@ if __name__ == '__main__':
    lub self.is_leader=False w przeciwnym przypadku
 4) Chcemy mieć możliwość uruchomienia tych wyborów z pojedynczego Candidate (nie tak byśmy musieli mieć referencje do wszystkich)
 """
-
